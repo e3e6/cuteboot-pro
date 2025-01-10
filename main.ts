c4c2e85f84c04874da32dcf749644c9ce08a2518
Alexander <xandrs.box@gmail.com>
2025-01-10 15:48:33 +0200

    start moving on shake

--- main.ts
+++ main.ts
@@ -1,10 +1,24 @@
+input.onGesture(Gesture.Shake, function () {
+    move = !(true)
+    if (move) {
+        CutebotPro.pwmCruiseControl(30, 30)
+        basic.showIcon(IconNames.Sword)
+    } else {
+        CutebotPro.pwmCruiseControl(0, 0)
+        basic.showIcon(IconNames.Square)
+    }
+})
 let sonar = 0
+let move = false
 basic.showIcon(IconNames.Happy)
+move = false
 basic.forever(function () {
     sonar = CutebotPro.ultrasonic(SonarUnit.Centimeters)
-    if (sonar > 2 && sonar < 20) {
-    	
-    } else {
-    	
+    if (move) {
+        if (sonar > 2 && sonar < 20) {
+            CutebotPro.trolleySteering(CutebotProTurn.LeftInPlace, 90)
+        } else {
+            CutebotPro.pwmCruiseControl(30, 30)
+        }
     }
 })
